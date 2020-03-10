be527c25e711a146d8544ecb61decb6fc677a81a
aka2029 <asaks22@gmail.com>
2020-03-10 23:39:01 +0530

    new commit

---
+++ 03_LanguageConstructs/printAllPrime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	//check for 2 3 4 .... N
+	int curNo = 2;
+	while (curNo <= N) {
+		//check if the number is prime
+		bool isPrime = true;	//assumption
+		int div = 2;			//divisor of curNo
+		while (div < curNo) {
+			//if div divides curNo, assumption was wrong
+			if (curNo % div == 0) {
+				//oops! update isPrime and break
+				isPrime = false;
+				break;
+			}
+			++div;
+		}
+		//Hmmm... I know whether the no is prime or not at this line
+		if (isPrime == true) {
+			cout << curNo << " ";	//space is must
+		}
+		++curNo;
+	}
+}
