be85fbaf314ff7b68cd03945bb154b2f5faaaa85
aka2029 <asaks22@gmail.com>
2020-03-10 23:38:05 +0530

    new commit

---
+++ 03_LanguageConstructs/cntDig.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	int nDig = 0;
+
+	while(n != 0){
+		int dig = n % 10;	//extracted the unit digit...redundant step
+		
+		++nDig;				//Oh!! I have one digit
+		n = n / 10;			//Now the number has shifted one digit to right
+		//So effectively, 123-->12 ;-)
+	}
+	cout << nDig;	
+}
