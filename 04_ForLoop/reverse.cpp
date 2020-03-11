79c5608f1ea6fc4362a704472460c7beb96ec9ae
aka2029 <asaks22@gmail.com>
2020-03-11 23:56:33 +0530

    new commit

---
+++ 04_ForLoop/reverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int N;
+	cin >> N;
+
+	//reverse
+	int reversedNum = 0;
+	while(N > 0){
+		int unitDigit = N % 10; 	//extract a digit
+		reversedNum = reversedNum * 10;	//shift to left
+		reversedNum = reversedNum + unitDigit;	//add the extracted digit
+		N = N / 10;	//remove the unit digit from the original number
+	}	
+	cout << reversedNum;
+
+	//HW: handle negatives
+
+	return 0; 
+}
