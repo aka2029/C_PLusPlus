8e5bba2799d6ad2459af62509a15ed2f419fcc49
aka2029 <asaks22@gmail.com>
2020-03-11 23:56:21 +0530

    new commit

---
+++ 04_ForLoop/reverseFor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int N;
+	cin >> N;
+	int reversedNo = 0;
+	for(; N > 0; N = N / 10){
+		reversedNo *= 10;
+		reversedNo += N % 10;
+	}	
+	cout << reversedNo << endl;
+}
