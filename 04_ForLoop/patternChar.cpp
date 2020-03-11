04746e422d164a5eb7649541683e936c5f4a8ce3
aka2029 <asaks22@gmail.com>
2020-03-11 23:58:06 +0530

    new commit

---
+++ 04_ForLoop/patternChar.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int N;
+	cin >> N;
+
+	for(int curRow = N; curRow >= 1; --curRow){
+		//print curRow in increasing order
+		char c = 'A';
+		for(int i = 0; i < curRow; ++i){
+			// c = c + i;
+			cout << c;
+			c++;
+		}
+
+		//print in decreasing order
+		for(int i = 0; i < curRow; ++i){
+			// c = c - i;	//WRONG	
+			c--;
+			cout << c;
+		}
+		cout << endl;
+	}
+	return 0;
+}
