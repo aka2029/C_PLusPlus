5c29457c205fa3a477db19fa007c1000393f1e2e
aka2029 <asaks22@gmail.com>
2020-03-13 10:29:09 +0530

    new commit

---
+++ 04_ForLoop/cntUnitDollar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char ch;
+	int cnt = 0;
+
+	while (true) {
+		// cin >> ch;
+		cin.get(ch);
+		if (ch == '$') {
+			break;
+		}
+
+		++cnt;
+	}
+	cout << cnt;
+}
