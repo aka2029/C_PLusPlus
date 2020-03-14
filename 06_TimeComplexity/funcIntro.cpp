9f5f96d9e9bce15d70097c650676c7bc17cc2eda
aka2029 <asaks22@gmail.com>
2020-03-14 23:18:23 +0530

    new commit

---
+++ 06_TimeComplexity/funcIntro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int M;	//no of times
+	cin >> M;
+
+	for(int curIt = 0; curIt < M; ++curIt){
+		//read N
+		//print N numbers
+		int N;
+		cin >> N;
+		for(int i = 0; i < N; ++i){
+			cout << i << " ";
+		}
+		cout << endl;
+	}	
+}
