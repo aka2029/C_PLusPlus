c1630767095740510198d43f8b6baad82c140212
aka2029 <asaks22@gmail.com>
2020-03-16 22:00:58 +0530

    new commit

---
+++ 07_Functions/factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+	int res = 1;
+	for(int i = 2; i <= n; ++i) res = res * i;
+	return res;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	int ans = factorial(n);	//calling/invocation
+	cout << ans;		
+}
