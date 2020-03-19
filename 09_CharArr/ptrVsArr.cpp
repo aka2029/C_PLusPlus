5581e63e9496cb82d943e1066142fbc3a198db5d
aka2029 <asaks22@gmail.com>
2020-03-19 16:57:38 +0530

    new commit

---
+++ 09_CharArr/ptrVsArr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	const char * ptr = "Welcome";	//read-only memory
+	char arr[] = "Welcome";	
+
+	arr[4] = 'a';
+	// ptr[4] = 'a';
+
+	cout << "X" << arr << "X" << endl;
+	cout << "X" << ptr << "X" << endl;
+	
+}
