5e87c2975349a97672217a784c47e732dda105af
aka2029 <asaks22@gmail.com>
2020-03-19 16:55:18 +0530

    new commit

---
+++ 09_CharArr/intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	// char str[7] = {'w', 'e', 'l', 'c', 'o', 'm', 'e'};
+	char str[7] = "Welcome";
+	char str[] = "Coding\nBlocks";
+	cout << "X" << str << "X" << endl;
+	cout << sizeof(str) << endl;
+
+	
+}
