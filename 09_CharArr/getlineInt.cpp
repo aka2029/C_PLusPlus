e57a8958896b17d19c5b350d28464b89a7322be4
aka2029 <asaks22@gmail.com>
2020-03-19 16:54:51 +0530

    new commit

---
+++ 09_CharArr/getlineInt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	cin.get();
+
+	char arr[10];
+	cin.getline(arr, 10, '\n');
+
+	cout << n << endl;
+	cout << "X" << arr << "X" << endl;
+}
