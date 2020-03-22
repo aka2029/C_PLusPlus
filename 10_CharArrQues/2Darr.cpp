87df578e3f2b1c3ac50fa8ead73db6f543d21598
aka2029 <asaks22@gmail.com>
2020-03-22 10:44:02 +0530

    new commit

---
+++ 10_CharArrQues/2Darr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    cout << arr << endl;    //address of first row
+    cout << arr + 1 << endl;    
+    cout << **(arr + 1) << endl;
+    cout << arr[0] << endl; //row 0 itself
+    cout  << *(arr[0]) << endl;
+    cout << &arr[0][0] << endl;
+    cout << arr[0] + 1 << endl;
+    cout << (&arr[0][0]) + 1 << endl;
+    cout << arr + 1 << endl;
+    cout << &arr[1][0] << endl;
+}
