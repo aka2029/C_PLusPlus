5406c33ca399b4b486a5e6b5754df68a0bf25c15
aka2029 <asaks22@gmail.com>
2020-03-26 22:44:37 +0530

    new commit

---
+++ 11_Recursion/displayArr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../helper.h"
+using namespace std;
+
+void printArray(int arr[], int si, int ei){
+    if (si > ei) return ;
+
+    cout << arr[si] << " ";
+    printArray(arr, si + 1, ei);
+}
+
+
+int main(){
+    int arr[10];
+    int n;
+    cin >> n;
+
+    inputArr(arr, n);
+
+    printArray(arr, 0, n - 1);
+
+}
