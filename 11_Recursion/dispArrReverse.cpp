28e29bd1d08fc8e37ec9b4d52f9495b1ff002971
aka2029 <asaks22@gmail.com>
2020-03-26 22:44:03 +0530

    new commit

---
+++ 11_Recursion/dispArrReverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../helper.h"
+using namespace std;
+
+void printRev(int arr[], int si, int ei){
+    //when no elements to print
+    if (si > ei) return;
+
+    cout << arr[ei] << " ";
+    printRev(arr, si, ei - 1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[10];
+    inputArr(arr, n);
+
+    printRev(arr, 0, n - 1);  
+}
