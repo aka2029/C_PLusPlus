a18c9a18b9c1d70110dfdbb2bc7cbf185421c13e
aka2029 <asaks22@gmail.com>
2020-03-31 13:48:07 +0530

    new commit

---
+++ 11_Recursion/printDecInc.cpp
@@ -0,0 +1,18 @@
+//5 4 3 2 1 1 2 3 4 5
+#include <iostream>
+using namespace std;
+
+void printDecInc(int n){
+    if (n <= 0) return;
+
+    cout << n << " ";
+    printDecInc(n - 1);
+    cout << n << " ";
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    printDecInc(n);    
+}
