1e15b6aca4b9b01ef057acf3145bbd4b5346a8f3
aka2029 <asaks22@gmail.com>
2020-03-31 13:48:41 +0530

    new commit

---
+++ 11_Recursion/printInc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//printInc takes one integer N and prints N integers [1, N]
+void printInc(int n) {
+    if (n == 0) return;
+
+    printInc(n - 1);
+    cout << n << " ";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    printInc(n);
+}
