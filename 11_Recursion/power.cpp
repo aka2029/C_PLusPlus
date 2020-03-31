7366ba486c737d1d78623ef4d71c1147606ac717
aka2029 <asaks22@gmail.com>
2020-03-31 13:47:36 +0530

    new commit

---
+++ 11_Recursion/power.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int computePow(int n, int x){
+    if (x <= 0) return 1;
+
+    int halfPow = computePow(n, x / 2);
+    int ans = halfPow * halfPow;
+
+    if (x % 2 == 1) ans = ans * n;
+    return ans;
+}
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    int ans = computePow(n , x);
+    cout << ans;   
+}
