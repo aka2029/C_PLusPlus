7b83ea3f513c9824b2d4c7dbeef6d30dda60d17b
aka2029 <asaks22@gmail.com>
2020-04-03 21:52:24 +0530

    new commit

---
+++ 16_StacksQueues/nodeTemplate.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node<T> * next;
+    Node(T d){
+        data = d;
+        next = NULL;
+    }
+};
+
+template <typename T, typename C> 
+class Pair{
+    T first;
+    C second;
+};
+
+int main() {
+    Pair<int, char> p;
+
+    Node<Pair<int, char> > n(p);
+
+}
