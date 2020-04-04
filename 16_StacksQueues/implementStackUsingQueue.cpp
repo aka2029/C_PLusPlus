28b5fa22cc02881cce73217702e894cf4be10c20
aka2029 <asaks22@gmail.com>
2020-04-04 22:42:29 +0530

    new commit

---
+++ 16_StacksQueues/implementStackUsingQueue.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class StackUsingQue{
+    queue<int> q1;
+    queue<int> q2;
+
+public:
+    StackUsingQue(){
+
+    }
+    void push(int d);
+    int pop();
+}
+
+
+int main() {
+    StackUsingQue s;
+    s.push(2);
+    s.pop();
+}
