db3d4cbb4662617137b3b2bbd4b4ae652e6b78c8
aka2029 <asaks22@gmail.com>
2020-04-09 01:28:14 +0530

    new commit

---
+++ 20_HashMaps/stlMaps.cpp
@@ -0,0 +1,31 @@
+#include <map>
+#include <iostream>
+using namespace std;
+
+int main() {
+    //insert in a map
+    //delete
+    //print a map
+
+    // map<int, string> m;
+    map<string, int> m;
+
+    m["abc"] = 44;
+    m["def"] = 34;
+
+    // cout << m["abcd"];
+    // if (m.find("abc")){
+    //     //iter to pair found
+    //     //m.end()
+
+    // }
+       
+
+    if (m["abcd"]){
+
+    }
+
+    if (m.find("abcd") != m.end()){
+
+    }
+}
