dee276188f4d89bbf687ee3df832db81ed7c349c
aka2029 <asaks22@gmail.com>
2020-04-09 20:12:59 +0530

    new commit

---
+++ 22_Graphs/string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+    string s = "Abcdef";
+    s[2] = '\0';
+
+    char  cs[] = "Abcdef";
+    cs[2] = '\0';
+
+
+ 
+    cout << "string " << s << endl;
+    cout << "char [] " << (cs) << endl;   
+}
